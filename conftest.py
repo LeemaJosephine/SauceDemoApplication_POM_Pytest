48721e08b61f778a61f88bf6abcc73d1899f719f
LeemaJosephine <leemajosephine3@gmail.com>
2026-03-27 14:29:52 +0530

    Browser options updated

--- conftest.py
+++ conftest.py
@@ -1,12 +1,12 @@
-# conftest.py
 import pytest
 import allure
 from selenium import webdriver
-from selenium.webdriver.edge.service import Service
-from selenium.webdriver.edge.options import Options
-from webdriver_manager.microsoft import EdgeChromiumDriverManager
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
 
 from TestData.data import SauceDemoData
+
 
 @allure.step("Take screenshot and attach to Allure report")
 def capture_screenshot(driver, step_name):
@@ -16,20 +16,27 @@ def capture_screenshot(driver, step_name):
         attachment_type=allure.attachment_type.PNG
     )
 
+
 @pytest.fixture
 def setup_browser():
     options = Options()
-    # MUST for Jenkins
-    options.add_argument("--headless=new")
+
+    # Jenkins safe options
+    #
+    # options.add_argument("--headless=new")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--disable-gpu")
     options.add_argument("--window-size=1920,1080")
 
-    driver = webdriver.Chrome(options=options)
+    driver = webdriver.Chrome(
+        service=Service(ChromeDriverManager().install()),
+        options=options
+    )
+
     driver.implicitly_wait(25)
     driver.get(SauceDemoData.url)
-    yield driver
-    driver.quit()
 
-    ##
+    yield driver
+
+    driver.quit()
